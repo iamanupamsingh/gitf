9e52a5e9dc3598e91abf5471766646ed878d89ba
Anupam Singh <anupam@Anupams-MacBook-Air.local>
2022-09-15 18:10:13 +0530

    first file

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+int
+main ()
+{
+  int a = 2;
+  int b = a + 1;
+
+  if ((a = 3) == b)
+    {
+      cout << a << endl;
+    }
+  else
+    {
+      cout << a + 10;
+    }
+}
